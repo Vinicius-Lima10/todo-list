5113f8cede7ccf17019b9681d98d951dc36cdd56
Vinicius-Lima10 <viniciusacelerazg@gmail.com>
2025-09-11 04:55:41 +0000

    Feat: pequenos ajustes e comentários

--- todo-list/java/src/TaskController.java
+++ todo-list/java/src/TaskController.java
@@ -8,14 +8,20 @@ public class TaskController {
     public void criarTarefa(String nome, String descricao,
                             int nivelPrioridade, String categoria,
                             Status status, LocalDate dataDeTermino) {
+
+        //Primeiro verifica se não tem outra com mesmo nome
         boolean existe = listaTarefa.stream()
                 .anyMatch(tarefa -> tarefa.getNome().equalsIgnoreCase(nome));
 
         if (existe) {
             throw new RuntimeException("Tarefa já existente de nome: " + nome);
         }
+
+        //Adiciona a tarefa se não existe
         listaTarefa.add(new  Tarefa(nome, descricao, nivelPrioridade,
                 categoria, status, dataDeTermino));
+
+        //Ordena pela prioridade
         listaTarefa.sort((t1, t2) -> listaSort(t1, t2));
     }
 
@@ -23,6 +29,8 @@ public class TaskController {
         return this.listaTarefa;
     }
 
+
+    //Busca por uma tarefa específica
     public Tarefa listarTarefa(String nome) {
         for(Tarefa tarefa: listaTarefa) {
             if(tarefa.getNome().equalsIgnoreCase(nome)) {
@@ -32,6 +40,7 @@ public class TaskController {
         return null;
     }
 
+    //Deleta tarefa
     public void deletarTarefa(String nome) {
         boolean remover = this.listaTarefa.removeIf(tarefa -> {
             if(tarefa.getNome().equalsIgnoreCase(nome)) {
@@ -43,16 +52,21 @@ public class TaskController {
             System.out.println("Tarefa não  encontrada");
         }
     }
+
+    //EDita taraefa
     public void editarTarefa(String nome, String novoNome,
                              String novaDescricao, Integer novaPrioridade,
                              String novaCategoria, Status novoStatus,
                              LocalDate novaData) {
+
+        //Obter a tarefa específica que queremos editar
         Tarefa tarefa = listarTarefa(nome);
         if (tarefa == null) {
             throw new RuntimeException("Tarefa não encontrada - " + nome);
         }
 
         if (novoNome != null && !tarefa.getNome().equalsIgnoreCase(novoNome)) {
+            //Se modifica o nome, pesquisa para ver se não tem nenhum igual
             boolean existe = listaTarefa.stream()
                     .anyMatch(t -> t.getNome().equalsIgnoreCase(novoNome));
             if (existe) {
@@ -61,6 +75,7 @@ public class TaskController {
             tarefa.setNome(novoNome);
         }
 
+        //Se alterar os campos, setamos os novos valores
         if (novaDescricao != null) tarefa.setDescricao(novaDescricao);
         if (novaPrioridade != null) tarefa.setNivelPrioridade(novaPrioridade);
         if (novaCategoria != null) tarefa.setCategoria(novaCategoria);
